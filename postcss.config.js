49df7c2484691771ee41d43c8d8f02a3daf92ed7
Craig Petterson <cpetterson@yozu.co.uk>
2020-03-11 09:07:30 +0000

    Project init

---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './app/views/**/*.html.erb'
+  ],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss')('./app/javascript/css/tailwind.js'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3
+    }),
+    ...process.env.NODE_ENV === 'production' ? [purgecss] : []
+  ]
+}
+
